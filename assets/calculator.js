3ce718c0ccd77149cae51cca82b615a61b728972
Lipho V <plus20charisma@gmail.com>
2016-03-16 08:46:57 -0700

    finish

--- assets/calculator.js
+++ assets/calculator.js
@@ -19,8 +19,21 @@
         $scope.calculate = function() {
 
             $scope.inc_wattage = ($scope.current_lum * $scope.calc.inc).toFixed(1);
+            $scope.hal_wattage = ($scope.current_lum * $scope.calc.hal).toFixed(1);
+            $scope.cfl_wattage = ($scope.current_lum * $scope.calc.cfl).toFixed(1);
+            $scope.led_wattage = ($scope.current_lum * $scope.calc.led).toFixed(1);
 
-        }
+            if ($scope.current_hrs > 24) {$scope.current_hrs = 24;}
+            else if ($scope.current_hrs < 0) {$scope.current_hrs  = 0;}
+            var cost = $scope.current_cost / 100;
+            var total_hrs = $scope.total_days * $scope.current_hrs;
+
+            $scope.inc_cost = ((($scope.inc_wattage * total_hrs) / 1000) * cost).toFixed(2);
+            $scope.hal_cost = ((($scope.hal_wattage * total_hrs) / 1000) * cost).toFixed(2);
+            $scope.cfl_cost = ((($scope.cfl_wattage * total_hrs) / 1000) * cost).toFixed(2);
+            $scope.led_cost = ((($scope.led_wattage * total_hrs) / 1000) * cost).toFixed(2);
+
+        };
 
         $scope.calculate();
 
